b06f5137cbd5bbe010384f27e4ba295c77201170
LootX8 <lootx8@BoloDeCimento.local>
2023-12-19 16:49:31 -0300

    funções de rolagem e invocação mais dinamicas e organizadas, comentários adicionados em main.lua,e teste de invocação de um mecha no primeiro quadrado adicionado

--- main.lua
+++ main.lua
@@ -9,23 +9,31 @@ local Dice = require("boardgame.dice")
 local Field = require("boardgame.field")
 
 function love.load()
-    Width = 1368
-    Height = 720
+    -- Get screen size of the current computer.
+    love.window.setMode(0, 0, {
+        resizable = false
+    })
+    Screen_Height = love.graphics.getHeight()
+    Screen_Width = love.graphics.getWidth()
+    -- Scale = math.ceil(Screen_Width / 1000)
 
+    -- Set game screen
+    Height = Screen_Height -- 720
+    Width = Screen_Width   -- 1368
     love.window.setMode(Width, Height, {
         resizable = false
     })
 
-    love.window.setTitle('MDW')
-
+    -- Set title, font and colors
+    love.window.setTitle('Robot Dice Wars')
     love.graphics.setFont(love.graphics.newFont("fonts/Pixeled.ttf", 16))
+    -- White = { 255, 255, 255 }
+    Red = { 255, 0, 0 }
 
-    White = {255, 255, 255}
-    Red = {255, 0, 0}
-
-    Fps = 60
-    Speed = 60
-    Border = 0
+    -- Set max fps value
+    MAX_FPS = 60
+    -- Speed = 60
+    -- Border = 0
 
     Can_Play = false
     Turn = 0
@@ -42,11 +50,13 @@ function love.load()
     Deck1 = {}
     Deck2 = {}
 
+    Player_1_Summon_Positions = { 195, 258, 321, 384, 447 }
+
     -- mecha1 = Mecha:new('sprite_5.png', 2, 89, 195)
     -- mecha2 = Mecha:new('sprite_10.png', 3, 89, 258)
     -- mecha3 = Mecha:new('sprite_8.png', 4, 89, 321)
     -- mecha4 = Mecha:new('sprite_12.png', 5, 89, 384)
-    Mecha5 = Mecha:new('sprite_10.png', 6, 89, 447)
+    -- Mecha5 = Mecha:new('sprite_10.png', 6, 89, 447)
 
     Dice = Dice:new(652, 590)
 
@@ -56,26 +66,26 @@ function love.load()
 end
 
 function love.update(dt)
-    love.timer.sleep(1 / Fps)
+    love.timer.sleep(1 / MAX_FPS)
 
     if (Can_Play) then
         Can_Play = false
 
-        print("Turno antes da jogada: " .. Turn)
+        -- print("Turno antes da jogada: " .. Turn)
+        Dice_Values = {}
+        for i = 0, 2 do
+            Dice_Values[i] = Dice:roll()
+        end
 
-        Dice_Value1 = Dice:roll()
-        Dice_Value2 = Dice:roll()
-        Dice_Value3 = Dice:roll()
+        -- print(Dice_Values[0])
+        -- print(Dice_Values[1])
+        -- print(Dice_Values[2])
 
-        print(Dice_Value1)
-        print(Dice_Value2)
-        print(Dice_Value3)
-
-        Level_Value = Verify_Dice_Result(Dice_Value1, Dice_Value2, Dice_Value3)
+        Level_Value = Verify_Dice_Result(Dice_Values[0], Dice_Values[1], Dice_Values[2])
 
         if Level_Value ~= 0 then
-
-            Do_Something(Level_Value)
+            Summon_Robot(Level_Value)
+            Level_Value = 0
         end
 
         Pass_Turn()
@@ -84,11 +94,13 @@ function love.update(dt)
     end
 end
 
-function Do_Something(Level_Value)
+function Summon_Robot(Level_Value)
     if (Turn == 0 and #Deck1 == 0) then
         print("Level_Value: " .. Level_Value)
 
         Choose_Row = true
+        line = 1
+        mecha = Choose_Robot(Level_Value, line)
 
         -- Receber input do jogador em qual linha ele quer colocar o mecha
 
@@ -129,26 +141,29 @@ function Do_Something(Level_Value)
     end
 end
 
+function Choose_Robot(Level_Value, line)
+    Summon_Position = Player_1_Summon_Positions[line]
+    -- table.insert(Deck1, Mecha:new('sprite_' .. Level_Value .. '.png', 2, 89, Summon_Position))
+    return Mecha:new('sprite_' .. Level_Value .. '.png', Level_Value, 89, Summon_Position)
+end
+
 function Verify_Dice_Result(dice_value1, dice_value2, dice_value3)
-    if ((Is_Equal(dice_value1, dice_value2) or Is_Equal(dice_valuel, dice_value3) or Is_Equal(dice_value2, dice_value3)) and
-        not ((dice_value1 == 6 and dice_value2 == 6) or (dice_value1 == 6 and dice_value3 == 6) or
-            (dice_value2 == 6 and dice_value3 == 6))) then
-
+    -- Verify if two dices are equal and different from six
+    -- return dice_value if a pair exists, return zero if not.
+    if (Are_Two_Dices_Equal(dice_value1, dice_value2, dice_value3) and
+            not Are_Two_Dices_Equal_Six(dice_value1, dice_value2, dice_value3)) then
         if Is_Equal(dice_value1, dice_value2) then
-            print("1 e 2 iguais: " .. dice_value1, dice_value2)
-
+            -- print("1 e 2 iguais: " .. dice_value1, dice_value2)
             return dice_value1
         end
 
         if Is_Equal(dice_value1, dice_value3) then
-            print("1 e 3 iguais: " .. dice_value1, dice_value3)
-
+            -- print("1 e 3 iguais: " .. dice_value1, dice_value3)
             return dice_value1
         end
 
         if Is_Equal(dice_value2, dice_value3) then
-            print("2 e 3 iguais: " .. dice_value2, dice_value3)
-
+            -- print("2 e 3 iguais: " .. dice_value2, dice_value3)
             return dice_value2
         end
     end
@@ -159,9 +174,33 @@ end
 function Is_Equal(value1, value2)
     if (value1 == value2) then
         return true
+    else
+        return false
     end
+end
 
-    return false
+function Are_Two_Dices_Equal(value1, value2, value3)
+    if (value1 == value2) then
+        return true
+    elseif (value1 == value3) then
+        return true
+    elseif (value2 == value3) then
+        return true
+    else
+        return false
+    end
+end
+
+function Are_Two_Dices_Equal_Six(value1, value2, value3)
+    if (value1 == value2) and value1 == 6 then
+        return true
+    elseif (value1 == value3) and value1 == 6 then
+        return true
+    elseif (value2 == value3) and value2 == 6 then
+        return true
+    else
+        return false
+    end
 end
 
 function Pass_Turn()
@@ -189,27 +228,31 @@ end
 function love.draw()
     Background:draw()
 
-    love.graphics.print({Red, "Tecle SPACE para rolar os dados"}, 472, 540)
-
-    if Choose_Row then
-        love.graphics.print({Red, "Tecle 1, 2, 3, 4 ou 5 para escolher a linha"}, 472, 560)
-
-        if input == "1" then
-            Row_Value = 1
-
-            Choose_Row = false
-        end
-    end
+    love.graphics.print({ Red, "Tecle SPACE para rolar os dados" }, 472, 540)
 
     for _, Block in ipairs(Blocks) do
         Block:draw()
+    end
+
+    if Choose_Row then
+        love.graphics.print({ Red, "Tecle 1, 2, 3, 4 ou 5 para escolher a linha" }, 472, 560)
+        -- if input ~= "space" then
+        --    line = tonumber(input)
+        -- end
+        --mecha = Choose_Robot(Level_Value, line)
+        Choose_Row = false
+        Draw_Mecha = true
+    end
+
+    if Draw_Mecha then
+        mecha:draw()
     end
 
     -- mecha1:draw()
     -- mecha2:draw()
     -- mecha3:draw()
     -- mecha4:draw()
-    Mecha5:draw()
+    -- Mecha5:draw()
 
     Dice:draw()
 end
